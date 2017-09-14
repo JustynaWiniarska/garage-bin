b1a936c63d6052bb558521eb9b3543fa4911720e
Justyna Field <justyna.field@gmail.com>
2017-09-14 11:45:16 -0600

    Configure environment and make a GET request for list of items

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static('public'))
@@ -11,6 +15,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html')
 });
+
+
+app.get('/api/v1/items', (request, response) => {
+  database('items').select()
+    .then(items => {
+      response.status(200).json(items);
+    })
+    .catch(error => {
+      response.status(500).json(error)
+    })
+})
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`The App is running on ${app.get('port')}.`);
