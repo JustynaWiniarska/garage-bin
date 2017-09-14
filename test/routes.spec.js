7ad9cbad978070f5b1e515e336c55e91edc8d89d
Justyna Field <justyna.field@gmail.com>
2017-09-14 16:25:05 -0600

    Start testing routes

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -32,6 +32,33 @@ describe('Client Routes', () => {
   });
 
 
-  
+  describe('API Routes', () => {
+  //   before((done) => {
+  //   database.migrate.latest()
+  //     .then(() => done())
+  //     .catch(error => console.log(error));
+  // });
+  //
+  // beforeEach((done) => {
+  //   database.seed.run()
+  //     .then(() => done())
+  //     .catch(error => console.log(error));
+  // });
 
+
+    describe('GET /api/v1/items', () => {
+        it('should return all of the items', (done) => {
+          chai.request(server)
+            .get('/api/v1/items')
+            .end((error, response) => {
+              response.should.have.status(200);
+              response.should.be.json;
+              response.body.should.be.a('array');
+
+              done();
+            });
+        });
+    })
+
+  })
 })
