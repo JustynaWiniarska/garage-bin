aa683f9fe491e8eb25f3374f5fe9df69415dc6e9
Justyna Field <justyna.field@gmail.com>
2017-09-14 12:14:37 -0600

    Append item names to the DOM

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+$(document).ready(() => {
+  fetchAllItems();
+})
 
 
 $('#door').on('click', () => {
@@ -9,3 +12,21 @@ $('#close-door').on('click', () => {
   $('.garage-items').remove();
   $('.img-container').fadeIn(3000);
 })
+
+
+const fetchAllItems = () => {
+  fetch('/api/v1/items')
+  .then(res => res.json())
+  .then(data => {
+    appendItemNames(data)
+  })
+  .catch(error => console.log('Error fetching items: ', error))
+}
+
+const appendItemNames = (folders) => {
+  folders.map(folder => {
+     $('#items-list').append(
+      `<h4>${folder.name}</h4>`
+     )
+  })
+}
